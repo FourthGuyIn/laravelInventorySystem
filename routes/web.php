061c4c060f75a4d24e2678bcd78f41fa17e746c2
FourthGuyIn <w0473727@nscc.ca>
2023-12-15 13:48:43 -0400

    Set up Controller Logic

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ItemController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/categories/create', [CategoryController::class, 'create']);
+Route::post('/categories', [CategoryController::class, 'store']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/categories/{id}/edit', [CategoryController::class, 'edit']);
+Route::patch('/categories/{id}', [CategoryController::class, 'update']);
+
+Route::get('/items/create', [ItemController::class, 'create']);
+Route::post('/items', [ItemController::class, 'store']);
+Route::get('/items', [ItemController::class, 'index']);
+Route::get('/items/{id}/edit', [ItemController::class, 'edit']);
+Route::patch('/items/{id}', [ItemController::class, 'update']);
+Route::get('/items/{id}/delete', [ItemController::class, 'confirmDelete']);
+Route::delete('/items/{id}', [ItemController::class, 'destroy']);
 
 Route::get('/', function () {
     return view('welcome');
